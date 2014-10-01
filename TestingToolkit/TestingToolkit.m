fcadc499fb71294afc0408e03cbcc8739e480f40
Christian Tietze <christian.tietze@gmail.com>
2014-10-01 17:24:44 +0200

    Initial Commit

---
+++ TestingToolkit/TestingToolkit.m
@@ -0,0 +1,13 @@
+//
+//  TestingToolkit.m
+//  TestingToolkit
+//
+//  Created by Christian Tietze on 01.10.14.
+//  Copyright (c) 2014 Christian Tietze. All rights reserved.
+//
+
+#import "TestingToolkit.h"
+
+@implementation TestingToolkit
+
+@end
---
+++ TestingToolkitTests/TestingToolkitTests.m
@@ -0,0 +1,34 @@
+//
+//  TestingToolkitTests.m
+//  TestingToolkitTests
+//
+//  Created by Christian Tietze on 01.10.14.
+//  Copyright (c) 2014 Christian Tietze. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TestingToolkitTests : XCTestCase
+
+@end
+
+@implementation TestingToolkitTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
