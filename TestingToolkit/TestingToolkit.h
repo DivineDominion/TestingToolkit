eb5d5af497bf536004991bab8ea0b41efbf1a78f
Christian Tietze <christian.tietze@gmail.com>
2018-09-08 12:03:58 +0200

    migrate TestingToolkit to a Cocoa Framework

---
+++ TestingToolkit/TestingToolkit.h
@@ -0,0 +1,21 @@
+//
+//  TestingToolkit.h
+//  TestingToolkit
+//
+//  Created by Christian on 08.09.18.
+//  Copyright © 2018 Christian Tietze. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for TestingToolkit.
+FOUNDATION_EXPORT double TestingToolkitVersionNumber;
+
+//! Project version string for TestingToolkit.
+FOUNDATION_EXPORT const unsigned char TestingToolkitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestingToolkit/PublicHeader.h>
+
+#import "CTKCalendarSupplier.h"
+#import "CTKNotificationCenter.h"
+#import "CTKUserDefaults.h"
